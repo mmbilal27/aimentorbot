7271789fa7bff761853463e9dc2b522a042c3685
mmbilal27 <mmbilal27@gmail.com>
2023-11-13 23:33:32 +0500

    model 4

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if 'entered_prompt' not in st.session_state:
 # Initialize the ChatOpenAI model
 chat = ChatOpenAI(
     temperature=0.5,
-    model_name="gpt-3.5-turbo",
+    model_name="gpt-4-1106-preview",
     openai_api_key=openapi_key, 
     max_tokens=100
 )
